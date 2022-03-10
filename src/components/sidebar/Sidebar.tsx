d3da1cda43f89951ba15839ddb90aff8f1a44c87
Kevin Chen <kevin@bloxroute.com>
2022-03-10 10:30:34 -0600

    can now copy to clipboard

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { Box, Button, Stack, Typography } from "@mui/material";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import PlayerList from "../player_list/PlayerList";
 
 interface SidebarProps {
@@ -16,9 +18,19 @@ export default function Sidebar(props: SidebarProps) {
           <Typography variant="h4" sx={{ textAlign: "center" }}>
             Game ID
           </Typography>
-          <Typography variant="body1" sx={{ textAlign: "center" }}>
-            {props.sessionId}
-          </Typography>
+          <Stack
+            direction="row"
+            spacing={1}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Typography variant="body1">{props.sessionId}</Typography>
+            <CopyToClipboard text={props.sessionId}>
+              <Button sx={{ minWidth: 36 }}>
+                <ContentCopyIcon />
+              </Button>
+            </CopyToClipboard>
+          </Stack>
         </Box>
         <Box>
           <Button
